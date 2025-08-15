89e907038dce7c8cc7c012fb3ec1f057c84c4598
Soumyajit Baul <jit.baul13@gmail.com>
2025-08-15 13:16:49 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,2 +1,23 @@
 console.log("Hello")
-console.log();
+
+const accountId = 144553 // can't be changed
+let accountEmail = "Baul@google.com"
+var accountPassword = "0013" // var should be avoided cz of block/functional scope issue
+accountLocation = "Kolkata" //possible but avoid
+let accountState;
+let isLoggedIn = false;
+
+// null => standalone value //type: object
+// sysmbol => unique
+
+// object 
+
+console.log(accountEmail)
+console.table([accountId,accountEmail,
+    accountPassword,accountLocation,accountState])
+console.log(typeof accountId);
+
+// alert (3 + 3) // can't use since it is node not browser
+
+//"use strict" // implies newer version, default
+
